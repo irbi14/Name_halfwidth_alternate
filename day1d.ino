50a5fb810832fb83895447d8b7289a53733b22a6
irbi14 <41074448+irbi14@users.noreply.github.com>
2019-06-08 14:59:31 +0530

    Add files via upload

---
+++ day1d.ino
@@ -0,0 +1,37 @@
+#include <LiquidCrystal.h>
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  lcd.begin(16, 2);
+}
+
+void loop() 
+{
+
+  for (int pc = 0; pc < 6; pc++)
+  {
+    if (pc == 5)
+    {
+      for (int pc = 5; pc > 0; --pc)
+      {
+        lcd.setCursor(pc, 0);
+        lcd.print("RISHI");
+        lcd.setCursor(11 - pc, 1);
+        lcd.print("RISHI");
+        delay(150);
+        lcd.clear();
+      }
+    }
+    else
+    {
+      lcd.setCursor(pc, 0);
+      lcd.print("RISHI");
+      lcd.setCursor(11 - pc, 1);
+      lcd.print("RISHI");
+      delay(150);
+      lcd.clear();
+    }
+  }
+}
+
